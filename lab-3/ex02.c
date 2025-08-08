853d724e57d1f655bc3fc14e30b2680c2a6eedfb
Risa87 <Risa.n260149@gmail.com>
2025-08-08 10:05:45 +0000

    complete lab-3

---
+++ lab-3/ex02.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+
+    if (num >=1 && num <=100)
+        if (num %2 == 0){
+            printf ("%d is even\n",num);
+           }
+        else{
+            printf ("%d is odd\n",num);
+           }
+        else{
+            printf("%d out of range\n",num);
+        }
+        return 0;
+}
---
+++ lab-3/risa.c
@@ -0,0 +1,11 @@
+#include(studio.h)
+int main()
+{
+    int a;
+    int b;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    printf("Enter a number: ");
+    
+    return 0;
+}
